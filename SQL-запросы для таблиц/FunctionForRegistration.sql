8cdf2f9d920d46d5d9ecb2526938272dad3bdd72
unknown <429281-4>
2024-01-29 16:21:43 +0300

    Lab12: RegProcedureSQL added

---
+++ SQL-запросы для таблиц/FunctionForRegistration.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [dbo].[Insert_User]
+        @email NVARCHAR(100),
+		@phone int,
+		@password NVARCHAR(20),
+		@FIO NVARCHAR(100)
+
+AS
+BEGIN
+       SET NOCOUNT ON;
+       IF EXISTS(SELECT email FROM Users WHERE email = @email)
+       BEGIN
+              SELECT -1 AS UserId -- Username exists.
+       END
+      
+       ELSE
+       BEGIN
+              INSERT INTO [Users]
+                        ([FIO],
+						 [email],
+                         [phone],
+                         [password],
+						 [role])
+              VALUES
+                        (@FIO,
+						 @email,
+						 @phone,
+						 @password,
+						 "Клиент")
+             select 1
+              --SELECT SCOPE_IDENTITY() AS UserId -- UserId                     
+     END
+END
